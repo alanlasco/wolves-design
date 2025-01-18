ba1ddccdeb1cdcc8b59ea6ec49181fdb72df26ca
alanlasco <61526254+alanlasco@users.noreply.github.com>
2025-01-18 00:43:58 -0300

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export function App() {
+  return (
+    <div className="App">
+      <h1>prueba</h1>
+    </div>
+  );
 }
 
 export default App;
